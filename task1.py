df2082f3a645417788fefe809943dda6fffa1e09
anastty <anastasiae107@gmail.com>
2024-02-21 15:58:47 +0300

    password = input("Введите пароль:") confirmpassword = input("Подтвердите пароль:") if password == confirmpassword:     print("Пароль принят:") else:     print("Пароль не принят:")

---
+++ task1.py
@@ -0,0 +1,6 @@
+password = input("Введите пароль:")
+confirmpassword = input("Подтвердите пароль:")
+if password == confirmpassword:
+    print("Пароль принят:")
+else:
+    print("Пароль не принят:")
