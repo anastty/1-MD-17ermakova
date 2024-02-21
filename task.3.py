61996eea5c1987fe55219d756193fa9c25ecff33
anastty <anastasiae107@gmail.com>
2024-02-21 16:49:58 +0300

    3 задание к уроку 21.02.24

--- task.3.py
+++ task.3.py
@@ -1,6 +1,6 @@
-def isleapyear(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-print (f"Год{year} - високосный")
+def isleapear(year):
+    if (year % 4 == 0 and year % 100 !=0) or (year % 400 ==0):
+        print (f"Год{year} - високосный")
 else:
 print("Этот год не високосный")
 year = int(input("Введите год:"))
