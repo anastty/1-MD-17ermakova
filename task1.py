7d42b73eab7fe68a4ab20e0ce70206aea7815383
anastty <159265110+anastty@users.noreply.github.com>
2025-04-28 02:22:18 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,6 +0,0 @@
-password = input("Введите пароль:")
-confirmpassword = input("Подтвердите пароль:")
-if password == confirmpassword:
-    print("Пароль принят:")
-else:
-    print("Пароль не принят:")
