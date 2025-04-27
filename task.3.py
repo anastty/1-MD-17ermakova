e7d3b95378c32f0451c793bc7ea16e0197add86d
anastty <159265110+anastty@users.noreply.github.com>
2025-04-28 02:21:33 +0300

    Delete task.3.py

--- task.3.py
+++
@@ -1,7 +0,0 @@
-def isleapear(year):
-    if (year % 4 == 0 and year % 100 !=0) or (year % 400 ==0):
-        print (f"Год{year} - високосный")
-else:
-print("Этот год не високосный")
-year = int(input("Введите год:"))
-isleapyear(year)
