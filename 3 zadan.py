87d32dc967d1df894e4d78c421ede579c7638145
anastty <159265110+anastty@users.noreply.github.com>
2025-04-28 02:18:40 +0300

    Delete 3 zadan.py

--- 3 zadan.py
+++
@@ -1,7 +0,0 @@
-days_of_week = ( 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье')
-num_weekend = int(input("Сколько выходных дней вы хотите? "))
-your_weekend_days = days_of_week[-num_weekend:]
-print("Ваши выходные дни:", ','.join( your_weekend_days ))
-
-your_work_days = days_of_week[:-num_weekend]
-print("Ваши рабочие дни:", ','.join( your_work_days ))
