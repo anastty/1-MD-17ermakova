1b1e4e729520c93dccdea3e34f9c554db4cac8a0
anastty/1-MD-17ermakova <anastaiae107@gmail.com>
2025-05-15 04:09:41 +0300

    ИГРА

--- игра.py
+++ игра.py
@@ -1,9 +1,8 @@
 
 """
 Platformer Game
-
-python -m arcade.examples.platform_tutorial.03_more_sprites
 """
+import math
 import arcade
 
 # Constants
@@ -17,17 +16,23 @@ TILE_SCALING = 0.5
 CHARACTER_SCALING = 0.20
 
 PLAYER_MOVEMENT_SPEED = 15
-JUMP_MAX_HEIGHT = 100
+JUMP_MAX_HEIGHT = 200
+
+PLAYER_X_SPEED = 5
+PLAYER_Y_SPEED = 5
+
+PLAYER_SPRITE_IMAGE_CHANGE_SPEED = 1
 
 class GameView(arcade.Window):
     """
     Main application class.
     """
 
-    def __init__(self): # __init__, а не init
+    def __init__(self):
         # Call the parent class and set up the window
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
+        # Variable to hold our texture for our player
         self.player_list = None
         self.wall_list = None
         self.player_texture = None
@@ -41,19 +46,29 @@ class GameView(arcade.Window):
 
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
 
+        self.key_right_pressed = False
+        self.key_left_pressed = False
 
-        # Variable to hold our texture for our player
+        self.collide = False
 
+        self.player_dy = PLAYER_Y_SPEED
+        self.player_dx = PLAYER_X_SPEED
+
+        self.gui_camera = None
+        self.score_text = None
+        self.total_time = 0
+
+        self.player_sprite_images_r = []
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
-        self.player_texture = arcade.load_texture("images/перс для игры.png")
+        self.player_texture = arcade.load_texture("images/перс для игры1.png")
 
         # Separate variable that holds the player sprite
         self.player_sprite = arcade.Sprite(self.player_texture)
         self.player_sprite.scale = CHARACTER_SCALING
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 115
+        self.player_sprite.center_x = 40
+        self.player_sprite.center_y = 90
 
         # SpriteList for our player
         self.player_list = arcade.SpriteList()
@@ -63,17 +78,17 @@ class GameView(arcade.Window):
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
 
         # Create the ground
-        for x in range(0, 1250, 64):
+        for x in range(-300, 1250, 64):
             wall = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=0.5)
             wall.center_x = x
             wall.center_y = 32
             self.wall_list.append(wall)
 
         # Put some crates on the ground
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+        coordinate_list = [[100, 600], [450, 850], [50, 1000], [300, 1400], [100, 1600], [350, 1800], [100, 2000], [200, 200], [400, 700]]
 
         for coordinate in coordinate_list:
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", scale=0.5)
+            wall = arcade.Sprite("images/platforma.png", scale=0.5)
             wall.center_x = coordinate[0]
             wall.center_y = coordinate[1]
             self.wall_list.append(wall)
@@ -81,6 +96,11 @@ class GameView(arcade.Window):
         self.camera = arcade.Camera2D()
 
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+
+        self.gui_camera = arcade.Camera2D()
+
+        for i in range(1, 5):
+            self.player_sprite_images_r.append(arcade.load_texture(f"images/перс для игры{i}.png"))
 
 
     def on_draw(self):
@@ -91,6 +111,12 @@ class GameView(arcade.Window):
 
         self.player_list.draw()
         self.wall_list.draw()
+
+        # Within on_draw
+        self.gui_camera.use()
+
+        arcade.Text(f"Max Height: {self.camera_max}", x = 0, y = 5).draw()
+        arcade.Text(f"Time: {self.total_time_print}", x = 0, y = 20).draw()
 
     def center_camera_to_player(self):
         screen_center_x = self.player_sprite.center_x
@@ -108,23 +134,63 @@ class GameView(arcade.Window):
             screen_center_y = self.camera.viewport_height / 2
 
         player_centered = screen_center_x, screen_center_y
+
         self.camera.position = player_centered
 
+    def player_movement(self):
+        if self.collide:
+            self.player_dy = 0
+
+        else:
+            self.player_dy = PLAYER_Y_SPEED
+            self.player_dx = PLAYER_X_SPEED
+
+        if self.key_left_pressed:
+            self.player_sprite.center_x -= self.player_dx
+        if self.key_right_pressed:
+            self.player_sprite.center_x += self.player_dx
+            self.player_sprite.texture = self.player_sprite_images_r[int(self.player_sprite.center_x / PLAYER_SPRITE_IMAGE_CHANGE_SPEED) % 4]
+
+        if self.player_jump:
+            self.player_sprite.center_y += self.player_dy
+            if self.player_sprite.center_y > self.jump_start + JUMP_MAX_HEIGHT:
+                self.player_jump = False
+        else:
+            if self.player_sprite.center_y >= 113:
+                self.player_sprite.center_y -= self.player_dy
+
+    def calculate_collision(self):
+        self.collide = False
+
+        #for block in self.wall_list:
+         #   if block.center_x + block.width / 2 >= self.player_sprite.center_x >= block.center_x - block.width / 2 and \
+         #       block.center_y + block.heigth / 2 >= self.player_sprite.center_y - self.player_sprite.height / 2 >= block.center_y - block.heigth / 2:
+         #            self.collide = True
+        for block in self.wall_list:
+            if (self.player_sprite.center_x + self.player_sprite.width / 5 >= block.center_x - block.width / 2 and \
+                    self.player_sprite.center_x - self.player_sprite.width / 4 <= block.center_x + block.width / 2) and \
+                    (self.player_sprite.center_y + self.player_sprite.height / 2 >= block.center_y - block.height / 2 and \
+                            self.player_sprite.center_y - self.player_sprite.height / 2 <= block.center_y + block.height / 2):
+                   self.collide = True
 
     def on_update(self, delta_time):
         """Movement and Game Logic"""
         self.center_camera_to_player()
         self.physics_engine.update()
-
-        self.camera.position = self.player_sprite.position
+        self.player_movement()
 
         if self.player_jump:
-            self.player_sprite.center_y += 2
-            if self.player_sprite.center_y > self.jump_start + JUMP_MAX_HEIGHT:
-                self.player_jump = False
+            self.collide = False
         else:
-            self.player_sprite.center_y -= 2
+            self.calculate_collision()
 
+        self.total_time += delta_time
+        ms, sec = math.modf(self.total_time)
+
+        minutes = int(sec) // 60
+        seconds = int(sec) % 60
+        msec = int(ms * 100)
+        self.total_time_print = f"{minutes:02d}:{seconds:02d}:{msec:02d}"
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
@@ -132,26 +198,23 @@ class GameView(arcade.Window):
             self.setup()
 
         if key == arcade.key.UP or key == arcade.key.W:
+           # self.player_sprite.center_y +-50
+
             self.player_jump = True
             self.jump_start = self.player_sprite.center_y
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+            self.player_sprite.center_y -= 50
         elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            self.key_left_pressed = True
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+            self.key_right_pressed = True
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key."""
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = 0
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.key_left_pressed = False
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = 0
+            self.key_right_pressed = False
 
 
 def main():
